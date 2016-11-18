908c79bc418f678e181e539ba758548138268505
williansmartins <professorwillians@gmail.com>
2016-11-18 18:30:49 -0200

    v1.10.0 - After e Before - gacp2

---
+++ src/com/williansmartins/dasdas.java
@@ -0,0 +1,32 @@
+package com.williansmartins;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class dasdas {
+	private String stringLocal;
+
+    @Before
+    public void inicializaLocal() {
+        System.out.println("Inicializou.");
+        this.stringLocal = "inicializada";
+    }
+
+    @Test
+    public void testInicializadaLocal1() {
+        Assert.assertEquals("A String deveria estar inicializada.", "inicializada", this.stringLocal);
+    }
+    
+    @Test
+    public void testInicializadaLocal2() {
+        Assert.assertEquals("A String deveria estar inicializada.", "inicializada", this.stringLocal);
+    }
+    
+    @After
+    public void setaNull() {
+        System.out.println("Setou nulo.");
+        this.stringLocal = null;
+    }
+}
