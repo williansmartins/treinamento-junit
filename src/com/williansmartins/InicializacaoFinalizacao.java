a833efcd455c4b76c5a5b095d17c6d301ad29f7e
Willians Martins <wsms@gft.com>
2020-01-09 17:05:16 -0300

    Update and rename dasdas.java to InicializacaoFinalizacao.java

--- src/com/williansmartins/InicializacaoFinalizacao.java
+++ src/com/williansmartins/InicializacaoFinalizacao.java
@@ -5,7 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class dasdas {
+public class InicializacaoFinalizacao {
 	private String stringLocal;
 
     @Before
